3a8d33315dafcef16dbf8e773b2d8e6f9113569c
DowsingPlants <103420899+DowsingPlants@users.noreply.github.com>
2023-10-13 16:37:15 +0200

    updatet page with css and js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('scroll', function() {
         const header = document.querySelector('header');
+        const headerText = header.querySelector('h1');
         if (window.scrollY > 50) {
             header.classList.add('smaller');
+            headerText.classList.add('smaller');
         } else {
             header.classList.remove('smaller');
+            headerText.classList.remove('smaller');
         }
     });
 });
